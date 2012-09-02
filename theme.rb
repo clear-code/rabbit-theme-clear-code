994e2a14b88bd3563ede68b9b700f69f1ef93c25
Kouhei Sutou <kou@clear-code.com>
2012-09-02 15:46:01 +0900

    Add license header

--- theme.rb
+++ theme.rb
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012  Kouhei Sutou <kou@clear-code.com>
+#
+# License: GPLv3+, GFDL and/or CC BY-SA 3.0
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 @font_family = find_font_family("MotoyaLMaru") || @font_family
 
